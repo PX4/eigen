PX4/eigen

commit 5d51a7f12c69138ed2a43df240bdf27a5313f7ce

Don't optimize the processing of the last rows of a matrix matrix product in cases that violate the assumptions made by the optimized code path.

Eigen/src/Core/products/GeneralBlockPanelKernel.h
@@ -1625,9 +1625,14 @@ void gebp_kernel<LhsScalar,RhsScalar,Index,DataMapper,mr,nr,ConjugateLhs,Conjuga
           prefetch(&blA[0]);
           const RhsScalar* blB = &blockB[j2*strideB+offsetB*nr];
 
-          if( (SwappedTraits::LhsProgress % 4)==0 )
+          // The following piece of code wont work for 512 bit registers
+          // Moreover it assumes that there is a half packet of the same size
+          // as nr (which is currently 4) for the return type.
+          typedef typename unpacket_traits<SResPacket>::half SResPacketHalf;
+          if ((SwappedTraits::LhsProgress % 4) == 0 &&
+              (SwappedTraits::LhsProgress <= 8) &&
+              unpacket_traits<SResPacketHalf>::size==4)
           {
-            // NOTE The following piece of code wont work for 512 bit registers
             SAccPacket C0, C1, C2, C3;
             straits.initAcc(C0);
             straits.initAcc(C1);

commit 0fa9e4a15cb93fb6a0ef54ada13979d38d120ab0

Fix performance regression in dgemm introduced by changeset 5d51a7f12c69138ed2a43df240bdf27a5313f7ce

Eigen/src/Core/products/GeneralBlockPanelKernel.h
@@ -1526,12 +1526,12 @@ void gebp_kernel<LhsScalar,RhsScalar,Index,DataMapper,mr,nr,ConjugateLhs,Conjuga
           const RhsScalar* blB = &blockB[j2*strideB+offsetB*nr];
 
           // The following piece of code wont work for 512 bit registers
-          // Moreover it assumes that there is a half packet of the same size
+          // Moreover, if LhsProgress==8 it assumes that there is a half packet of the same size
           // as nr (which is currently 4) for the return type.
           typedef typename unpacket_traits<SResPacket>::half SResPacketHalf;
           if ((SwappedTraits::LhsProgress % 4) == 0 &&
               (SwappedTraits::LhsProgress <= 8) &&
-              unpacket_traits<SResPacketHalf>::size==4)
+              (SwappedTraits::LhsProgress!=8 || unpacket_traits<SResPacketHalf>::size==nr))
           {
             SAccPacket C0, C1, C2, C3;
             straits.initAcc(C0);