463ec866483806640c0a589afa427193a4599e8e
Sebastien Boisvert <seb@boisvert.info>
2020-06-09 18:15:52 -0400

    Fix #1757: remove the word 'suicide'

--- test/geo_hyperplane.cpp
+++ test/geo_hyperplane.cpp
@@ -117,7 +117,7 @@ template<typename Scalar> void lines()
       VERIFY_IS_APPROX(result, center);
 
     // check conversions between two types of lines
-    PLine pl(line_u); // gcc 3.3 will commit suicide if we don't name this variable
+    PLine pl(line_u); // gcc 3.3 will crash if we don't name this variable.
     HLine line_u2(pl);
     CoeffsType converted_coeffs = line_u2.coeffs();
     if(line_u2.normal().dot(line_u.normal())<Scalar(0))
