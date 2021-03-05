60452431412b27517207b002094176a7dc06271c
Antonio Sanchez <cantonios@google.com>
2021-03-05 14:29:37 -0800

    Revert stack allocation limit change that crept in.
    This was accidentally introduced when copying changes between repos.

--- Eigen/src/Core/util/Macros.h
+++ Eigen/src/Core/util/Macros.h
@@ -51,11 +51,7 @@
 
 #ifndef EIGEN_STACK_ALLOCATION_LIMIT
 // 131072 == 128 KB
-#if defined(__AVX512F__)
-  #define EIGEN_STACK_ALLOCATION_LIMIT 0
-#else
-  #define EIGEN_STACK_ALLOCATION_LIMIT 16384
-#endif
+#define EIGEN_STACK_ALLOCATION_LIMIT 131072
 #endif
 
 //------------------------------------------------------------------------------------------
