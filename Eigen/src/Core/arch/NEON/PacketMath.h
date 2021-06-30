94e2250b36f22e821507b4e6e47ce36236bf65da
大河メタル <metarutaiga@gmail.com>
2021-06-30 04:09:46 +0000

    Correct declarations for aarch64-pc-windows-msvc
    (cherry picked from commit c81da59a252b3479753b2eada26ee0cf46280bd0)

--- Eigen/src/Core/arch/NEON/PacketMath.h
+++ Eigen/src/Core/arch/NEON/PacketMath.h
@@ -32,7 +32,7 @@ namespace internal {
 #endif
 #endif
 
-#if EIGEN_COMP_MSVC
+#if EIGEN_COMP_MSVC_STRICT
 
 // In MSVC's arm_neon.h header file, all NEON vector types
 // are aliases to the same underlying type __n128.
@@ -78,7 +78,7 @@ typedef uint32x4_t                           Packet4ui;
 typedef int64x2_t                            Packet2l;
 typedef uint64x2_t                           Packet2ul;
 
-#endif // EIGEN_COMP_MSVC
+#endif // EIGEN_COMP_MSVC_STRICT
 
 EIGEN_STRONG_INLINE Packet4f shuffle1(const Packet4f& m, int mask){
   const float* a = reinterpret_cast<const float*>(&m);
