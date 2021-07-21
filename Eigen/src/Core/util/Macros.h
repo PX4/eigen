c0c7b695cd588e1694369dfc00f540f914434ed2
Antonio Sanchez <antonios@ece.ubc.ca>
2021-07-21 12:41:29 -0700

    Fix assignment operator issue for latest MSVC+NVCC.
    Details are scattered across #920, #1000, #1324, #2291.
    
    Summary: some MSVC versions have a bug that requires omitting explicit
    `operator=` definitions (leads to duplicate definition errors), and
    some MSVC versions require adding explicit `operator=` definitions
    (otherwise implicitly deleted errors).  This mess tries to cover
    all the cases encountered.
    
    Fixes #2291.
    
    
    (cherry picked from commit 9816fe59b47dc4c07967b5ee93a8e8aaa6e9c308)

--- Eigen/src/Core/util/Macros.h
+++ Eigen/src/Core/util/Macros.h
@@ -1185,8 +1185,12 @@ namespace Eigen {
   #define EIGEN_USING_STD(FUNC) using std::FUNC;
 #endif
 
-#if EIGEN_COMP_MSVC_STRICT && (EIGEN_COMP_MSVC < 1900 || EIGEN_COMP_NVCC)
-  // for older MSVC versions, as well as 1900 && CUDA 8, using the base operator is sufficient (cf Bugs 1000, 1324)
+#if EIGEN_COMP_MSVC_STRICT && (EIGEN_COMP_MSVC < 1900 || (EIGEN_COMP_MSVC == 1900 && EIGEN_COMP_NVCC))
+  // For older MSVC versions, as well as 1900 && CUDA 8, using the base operator is necessary,
+  //   otherwise we get duplicate definition errors
+  // For later MSVC versions, we require explicit operator= definition, otherwise we get
+  //   use of implicitly deleted operator errors.
+  // (cf Bugs 920, 1000, 1324, 2291)
   #define EIGEN_INHERIT_ASSIGNMENT_EQUAL_OPERATOR(Derived) \
     using Base::operator =;
 #elif EIGEN_COMP_CLANG // workaround clang bug (see http://forum.kde.org/viewtopic.php?f=74&t=102653)
