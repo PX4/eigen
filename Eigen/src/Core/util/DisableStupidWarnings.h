7ea4adb5f07ee762fd9d8c3a9cde8edf4e6dce44
Antonio Sanchez <cantonios@google.com>
2021-09-23 13:43:18 -0700

    Disable another device warning
    (cherry picked from commit e9e90892fecb4bebe6473e9de491bfcd6c0de37f)

--- Eigen/src/Core/util/DisableStupidWarnings.h
+++ Eigen/src/Core/util/DisableStupidWarnings.h
@@ -93,6 +93,7 @@
   #pragma diag_suppress 2735
   #pragma diag_suppress 2737
   #pragma diag_suppress 2739
+  #pragma diag_suppress 2976
   #pragma diag_suppress 2979
   // Disable the "// __device__ annotation is ignored on a function(...) that is
   //              explicitly defaulted on its first declaration" message.
